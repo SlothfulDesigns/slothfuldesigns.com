9131c05002f6f6ba11fdbeb6be5d7d04689cfd7a
Csaba <mognoose@gmail.com>
2016-10-29 22:16:03 +0300

    content layout tweak

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ document.getElementById('banner').style.backgroundImage = 'url(sloth.jpg)';
 
 document.getElementById('banner').innerHTML = '<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>';
 
-document.getElementById('container').innerHTML = '<h1>About</h1><p>Slothful Designs is a game developement company based in Helsinki, Finland. Currently we are working on our own game engine.</p>';
+document.getElementById('container').innerHTML = '<h1>About</h1><p>Slothful Designs is a game developement company based in Helsinki, Finland.<br/>Currently we are working on our own game engine.</p>';
