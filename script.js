57af38fbf70db5a6c5aad6ca24aacba5b044e721
Csaba <mognoose@gmail.com>
2016-10-29 21:28:51 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('header').innerHTML = '<img src="sloth-logo.svg" id="logo"/>';
+
+document.getElementById('menu').innerHTML = '<a href="#" class="menu">HOME</a><a href="#" class="menu">GAMES</a><a href="#" class="menu">ABOUT</a>';
+
+document.getElementById('banner').style.backgroundImage = 'url(sloth.jpg)';
+
+document.getElementById('banner').innerHTML = '<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>';
+
+document.getElementById('container').innerHTML = '<h1>About</h1><p>Slothful Designs is a game developement company based in Helsinki, Finland. Currently we are working on our own game engine.</p>';
