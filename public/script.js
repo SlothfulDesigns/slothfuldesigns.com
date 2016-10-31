0008bb19f44f460c798a7287d2cc58d1104e1f95
Csaba <mognoose@gmail.com>
2016-10-31 13:17:42 +0200

    Moar dynamicisation

--- public/script.js
+++ public/script.js
@@ -1,15 +1,41 @@
+function pageChange(page){
+
+var home = {
+	quote:'<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>',
+	img:'sloth.jpg',
+	content:'<h1>Welcome to Slothful Designs</h1><p>Slothful Designs is a game development company based in Helsinki, Finland.<br/>Currently we are working on our own game engine.</p>'
+	}
+
+var games = {
+	quote:'<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>',
+	img:'sloth.jpg',
+	content:'<h1>Games</h1><p>Coming soon... Stay tuned!</p>'
+	}
+
 var about = {
 	quote:'<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>',
 	img:'sloth.jpg',
 	content:'<h1>About</h1><p>Slothful Designs is a game development company based in Helsinki, Finland.<br/>Currently we are working on our own game engine.</p>'
 	}
 
-var current = about;
+var pages = {
+	home: home,
+	games: games,
+	about: about
+	}
 
+	var current = pages[page];
+
+	return current;
+
+
+	}
+
+var current = pageChange('home');
 
 document.getElementById('header').innerHTML = '<img src="sloth-logo.svg" id="logo"/>';
 
-document.getElementById('menu').innerHTML = '<a href="#" class="menu">HOME</a><a href="#" class="menu">GAMES</a><a href="#" class="menu">ABOUT</a>';
+document.getElementById('menu').innerHTML = '<a href="#" class="menu">HOME</a><a href="#" class="menu">GAMES</a><a href="#" id="about" class="menu">ABOUT</a>';
 
 document.getElementById('banner').style.backgroundImage = 'url('+current['img']+')';
 
