93f01e309fc63445c07ab000a3c4b78a9a9ddb89
Csaba <mognoose@gmail.com>
2016-11-02 10:04:20 +0200

    making page changing work

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 function loadContent(page){
 
+
 	var home = {
 		quote:'<p id="bannerTxt">&quot;I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.&quot;<br>-Bill Gates</p>',
 		img:'sloth.jpg',
@@ -26,18 +27,21 @@ function loadContent(page){
 
 	var current = pages[page];
 
-	return current;
-
-	}
-
-var current = loadContent('home');
-
-document.getElementById('header').innerHTML = '<img src="sloth-logo.svg" id="logo"/>';
-
-document.getElementById('menu').innerHTML = '<a href="#" class="menu">HOME</a><a href="#" class="menu">GAMES</a><a href="#" class="menu">ABOUT</a>';
 
 document.getElementById('banner').style.backgroundImage = 'url('+current['img']+')';
 
 document.getElementById('banner').innerHTML = current['quote'];
 
 document.getElementById('container').innerHTML = current['content'];
+
+}
+
+	var home = "home";
+	var games = "games";
+	var about = "about";
+
+document.getElementById('header').innerHTML = '<img src="sloth-logo.svg" id="logo"/>';
+
+document.getElementById('menu').innerHTML = '<a href="#" onClick="loadContent(home)" class="menu">HOME</a><a href="#" onClick="loadContent(games)" class="menu">GAMES</a><a href="#" onClick="loadContent(about)" class="menu">ABOUT</a>';
+
+window.onLoad = loadContent('home');
